422142ed8f3ace373b3d49471422702f3c9f32f8
Antony Koch <ant@iamkoch.com>
2025-08-24 21:19:26 +0100

    feat: integrate t.Run support into main library
    Merges v2 functionality into the main library, adding proper test
    integration with t.Run for each step and teardown. Removes v2
    directory as functionality is now unified.

--- lib.go
+++ lib.go
@@ -2,35 +2,19 @@ package ensure
 
 import (
 	"context"
-	"fmt"
 	"testing"
 )
 
-type ScenarioPrinter interface {
-	Write(s string)
-}
-
-type fmtPrinter struct {
-}
-
-func (d fmtPrinter) Write(s string) {
-	fmt.Println(s)
-}
-
-var (
-	Printer ScenarioPrinter = &fmtPrinter{}
-)
-
-func That(s string, f func(s *Scenario), t *testing.T) {
-	Printer.Write("Scenario: " + s)
-	scn := &Scenario{
-		t: t,
-	}
-	f(scn)
-	for _, f2 := range scn.teardownMethods {
-		Printer.Write("Tearing down " + f2.name)
-		f2.f()
-	}
+func That(scenarioName string, scenarioFunc func(s *Scenario), t *testing.T) {
+	t.Run("Scenario__"+scenarioName, func(t *testing.T) {
+		scenario := &Scenario{t: t}
+		scenarioFunc(scenario)
+		for _, teardown := range scenario.teardownMethods {
+			t.Run("Teardown of "+teardown.name, func(t *testing.T) {
+				teardown.f()
+			})
+		}
+	})
 }
 
 type Scenario struct {
@@ -38,55 +22,59 @@ type Scenario struct {
 	t               *testing.T
 }
 
-func (s2 *Scenario) Given(s string, f func()) *Scenario {
-	Printer.Write(" Given " + s)
-	f()
-	return s2
+func (s *Scenario) Given(stepName string, stepFunc func(t *testing.T)) *Scenario {
+	s.t.Run("Given "+stepName, func(t *testing.T) {
+		stepFunc(t)
+	})
+	return s
 }
 
-func (s2 *Scenario) And(s string, f func()) *Scenario {
-	Printer.Write("  And " + s)
-	f()
-	return s2
+func (s *Scenario) And(stepName string, stepFunc func(t *testing.T)) *Scenario {
+	s.t.Run("And "+stepName, func(t *testing.T) {
+		stepFunc(t)
+	})
+	return s
 }
 
-func (s2 *Scenario) When(s string, f func()) *Scenario {
-	Printer.Write(" When " + s)
-	f()
-	return s2
+func (s *Scenario) When(stepName string, stepFunc func(t *testing.T)) *Scenario {
+	s.t.Run("When "+stepName, func(t *testing.T) {
+		stepFunc(t)
+	})
+	return s
 }
 
-func (s2 *Scenario) Background(s string, f func()) *Scenario {
-	Printer.Write(" Background " + s)
-	f()
-	return s2
+func (s *Scenario) Background(stepName string, stepFunc func(t *testing.T)) *Scenario {
+	s.t.Run("Background of "+stepName, func(t *testing.T) {
+		stepFunc(t)
+	})
+	return s
 }
 
-func (s2 *Scenario) Then(s string, f func()) *Scenario {
-	Printer.Write(" Then " + s)
-	f()
-	return s2
+func (s *Scenario) Then(stepName string, stepFunc func(t *testing.T)) *Scenario {
+	s.t.Run("Then "+stepName, func(t *testing.T) {
+		stepFunc(t)
+	})
+	return s
 }
 
 // Teardown adds a function to be called when the scenario ends.
-// The function is passed a context that is cancelled when the scenario ends.
-func (s2 *Scenario) Teardown(s string, ctx context.Context, f func(ctx context.Context)) *Scenario {
-	s2.addTearDown(s, func() {
-		ctx.Done()
-		f(ctx)
+// The function is passed a context that is canceled when the scenario ends.
+func (s *Scenario) Teardown(stepName string, ctx context.Context, teardownFunc func(ctx context.Context)) *Scenario {
+	s.addTearDown(stepName, func() {
+		teardownFunc(ctx)
 	})
-	return s2
+	return s
 }
 
-func (s2 *Scenario) addTearDown(s string, f func()) {
-	s2.teardownMethods = append(s2.teardownMethods, tearDown{
-		name: s,
+func (s *Scenario) addTearDown(name string, f func()) {
+	s.teardownMethods = append(s.teardownMethods, tearDown{
+		name: name,
 		f:    f,
 	})
 }
 
-func (s2 *Scenario) NotImplemented() {
-	s2.t.Fatal("Not implemented")
+func (s *Scenario) NotImplemented() {
+	s.t.Fatal("Not implemented")
 }
 
 type tearDown struct {
